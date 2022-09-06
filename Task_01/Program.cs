5328f71fb602f42cce2aa67092641139ec95d2f5
Slava <you@example.com>
2022-09-06 14:54:56 +0300

    Task_01

---
+++ Task_01/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("введите первое число число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второечисло число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int square = num2* num2;
+if(square==num1) {
+    Console.Write($"Число {num1} является квадратом {num2}");   
+}
+else {
+    Console.Write($"Число {num1} не является квадратом {num2}");
+}
+
+
+
+
+
+
