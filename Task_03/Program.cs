d18f78e1b180bce6c770e537bbccb196e13c1d4f
Slava <you@example.com>
2022-09-06 16:03:38 +0300

    Add Task_03

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -5,27 +5,11 @@
 Console.Clear();
 Console.Write("Введите номер от 1 до 7:");
 string day = Console.ReadLine();
-if (day=="1") {
-    Console.Write ("Понедельник");
-}
-else if (day=="2") {
-    Console.Write ("Вторник");
-}
-else if (day=="3") {
-    Console.Write ("Среда");
-}
-else if (day=="4") {
-    Console.Write ("Четверг");
-}
-else if (day=="5") {
-    Console.Write ("Пятница");
-}
-else if (day=="6") {
-    Console.Write ("Суббота");
-}
-else if (day=="7") {
-    Console.Write ("Воскресенье");
-}
-else {
-    Console.Write("Дня недели с этим номером не существует");
-}
+if (day=="1") Console.Write ("Понедельник");
+else if (day=="2") Console.Write ("Вторник");
+else if (day=="3")  Console.Write ("Среда");
+else if (day=="4")  Console.Write ("Четверг");
+else if (day=="5") Console.Write ("Пятница");
+else if (day=="6") Console.Write ("Суббота");
+else if (day=="7") Console.Write ("Воскресенье");
+else Console.Write("Дня недели с этим номером не существует");
