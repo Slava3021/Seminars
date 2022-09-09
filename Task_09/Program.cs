e4e9caf4bdcfbd6e4607d6b7abaf06d97d4333ac
Slava <you@example.com>
2022-09-09 13:59:26 +0300

    add task_0_9

--- Task_09/Program.cs
+++ Task_09/Program.cs
@@ -1,8 +1,22 @@
 ﻿int number = new Random().Next(10,100);
-int firstDigit = number/10;
-int secondDigit = number % 10;
-// if (firstDigit > secondDigit) Console.WriteLine($"наибольшая цифра числа {number} ->{firstDigit}");
-// else Console.WriteLine($"наибольшая цифра числа {number} ->{secondDigit}");
+// int firstDigit = number/10;
+// int secondDigit = number % 10;
+// // if (firstDigit > secondDigit) Console.WriteLine($"наибольшая цифра числа {number} ->{firstDigit}");
+// // else Console.WriteLine($"наибольшая цифра числа {number} ->{secondDigit}");
+// if(firstDigit==secondDigit) Console.WriteLine($"цифры равны");
+// else{
+// int max = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"наибольшая цифра числа {number} ->{max}");
+// }
 
-int max = firstDigit > secondDigit ? firstDigit : secondDigit;
-Console.WriteLine($"наибольшая цифра числа {number} ->{max}");
+int MaxDigit(int num)
+{
+int firstDigit = num/10;
+int secondDigit = num % 10; 
+if (firstDigit==secondDigit) return 0;
+return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+int maxDigit =MaxDigit(number);
+string result = maxDigit >0 ? maxDigit.ToString() : "Цифры равны";
+Console.WriteLine($"наибольшая цифра числа {number} -> {result}");
+
