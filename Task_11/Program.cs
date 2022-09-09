1f3ec9528b6a83c579dc8d4cc649f36a42391e1e
Slava <you@example.com>
2022-09-09 15:34:14 +0300

    "

---
+++ Task_11/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = new Random().Next(100,1000);
+int MaxDigit(int num)
+{
+    int firstDigit = num / 100 *10;
+    int secondDigit = num % 10;
+    return firstDigit + secondDigit;
+}
+int result = MaxDigit(number);
+Console.WriteLine($" {number} -> {result}");
