9c530f6c576e9e1aa2fe69f9b7e278bbcb8af017
Slava <you@example.com>
2022-09-09 13:32:19 +0300

    add task_0_9

--- Task_09/Program.cs
+++ Task_09/Program.cs
@@ -1,5 +1,8 @@
 ﻿int number = new Random().Next(10,100);
 int firstDigit = number/10;
 int secondDigit = number % 10;
-if (firstDigit > secondDigit) Console.WriteLine($"наибольшая цифра числа {number} ->{firstDigit}");
-else Console.WriteLine($"наибольшая цифра числа {number} ->{secondDigit}");
+// if (firstDigit > secondDigit) Console.WriteLine($"наибольшая цифра числа {number} ->{firstDigit}");
+// else Console.WriteLine($"наибольшая цифра числа {number} ->{secondDigit}");
+
+int max = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.WriteLine($"наибольшая цифра числа {number} ->{max}");
