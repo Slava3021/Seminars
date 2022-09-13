804643d1ecdbfda9bd0cc827f977f2ebed745bd1
Slava <you@example.com>
2022-09-13 11:12:52 +0300

    ghjj

--- Task_12/Program.cs
+++ Task_12/Program.cs
@@ -3,19 +3,15 @@ Console.WriteLine("введите первое число:");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите второе число:");
 int num2 = Convert.ToInt32(Console.ReadLine());
-if (num1 > num2)
-{
-    if (num1 % num2 == 0)
-    {
+if(num1>num2){
+    if(num1 % num2==0){
         Console.WriteLine($"{num1} кратно {num2}");
     }
-    else
-    {
+    else{
         int ostatok = num1 % num2;
         Console.WriteLine($"{num1}  не кратно {num2}, остаток от деления равен:{ostatok}");
     }
 }
-else
-{
-    Console.WriteLine($"Ошибка!!!Нужно чтобы первое число было больше чем второе");
+else{
+     Console.WriteLine($"Ошибка!!!Нужно чтобы первое число было больше чем второе");
 }
