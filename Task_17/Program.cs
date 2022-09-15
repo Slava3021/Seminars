f64c943bbc8f588db563ddf04faa861c501c6423
Slava <you@example.com>
2022-09-15 11:27:50 +0300

    17 и 18 задачи

---
+++ Task_17/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите координаты точки");
+Console.Write("X:  ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y:  ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+string Quarte(int xc, int yc)
+{
+    if (xc > 0 && yc > 0) return "Первая четверть";
+    if (xc < 0 && yc > 0) return "Вторая  четверть";
+    if (xc < 0 && yc < 0) return "Третья четверть";
+    if (xc > 0 && yc < 0) return "Четвертая четверть";
+    return "введены некоректные координаты";
+}
+string result = Quarte(x,y);
+Console.WriteLine(result);
---
+++ Task_18/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите номер четверти");
+// Console.Write("X:  ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+string Quarte(int xc)
+{
+    if (xc == 1) return "x>0, y>0";
+    if (xc == 2) return "x<0, y>0";
+    if (xc == 3) return "x<0, y<0";
+    if (xc == 4) return "x>0, y<0";
+    return "Введите число от 1 до 4";
+}
+string result=Quarte(x);
+Console.WriteLine(result);
