ac0478c7d0d4c508a8028e0b781c013f15524f65
Slava <you@example.com>
2022-09-23 14:02:43 +0300

    seminar5

---
+++ Task_36/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+Console.Clear();
+int[] CreateArrayRndint(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+int[] Sum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0) sum=sum+array[i];
+    }
+    Console.WriteLine($"Сумма четных элементов = {sum}");
+    return array;
+}
+int[] arr = CreateArrayRndint(5, -100, 100);
+PrintArray(arr);
+int[] result = Sum(arr);
+Console.WriteLine($"{result}");
---
+++ Task_38/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+double[] CreateArrayRndintDouble(int size, int min, int max)
+{
+    double[] array = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Math.Round(rnd.NextDouble() * 100, 1);
+    }
+    return array;
+}
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}: ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+double[] Difference(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (max < array[i]) max = array[i];
+        if (min > array[i]) min = array[i];
+    }
+    double diff = max - min;
+
+    Console.WriteLine($"Разница между максимальным и минимальным  = {diff}");
+    return array;
+}
+double[] arr = CreateArrayRndintDouble(5, 0, 100);
+PrintArray(arr);
+double[] result = Difference(arr);
+Console.WriteLine($"{result}");
