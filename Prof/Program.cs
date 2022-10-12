b653cbd174821e9ca7bc2df92043b51308bc564d
Slava <you@example.com>
2022-10-12 19:21:30 +0300

    profoient

---
+++ Prof/Program.cs
@@ -0,0 +1,67 @@
+﻿Console.Clear();
+string[] CreateArrayString(int size)
+{
+    string[] stringArray = new string[size ];
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.WriteLine("Ввудите несколько символов");
+        stringArray[i] = Console.ReadLine();
+    }
+    return stringArray;
+}
+
+
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]} ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+
+
+
+
+
+
+string[] arr = CreateArrayString(5);
+
+PrintArray(arr);
+Console.WriteLine();
+string[] LesseQuallyThree(string[] array)
+{
+    int count = 0;
+    int n = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            count++;
+            
+        }
+    }
+      string[] stringArray = new string[count];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            stringArray[n] = array[i];
+            n++;
+        }
+    }
+
+    return stringArray;
+}
+
+
+
+
+string[] result = LesseQuallyThree(arr);
+PrintArray(result);
+
+
+
---
+++ Task_53/Program.cs
@@ -0,0 +1,65 @@
+﻿
+int[,] CreateMatrixRndint(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],5}, ");
+            else Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine(" ]");
+    }
+
+}
+int[,] CreateMatrixChange(int[,] matrix)
+{
+    int temp = default;
+    // linesCount = matrix.GetLength(0);
+    // columnsCount = matrix.GetLength(1);
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == 0)
+            {
+                temp = matrix[i, j];
+                matrix[i, j] = matrix[matrix.GetLength(0)-1, j];
+                matrix[matrix.GetLength(0)-1, j] = temp;
+            }
+
+        }
+        
+    }
+    return matrix;
+}
+
+
+
+
+
+
+
+
+
+int[,] array2D = CreateMatrixRndint(3, 4, 1, 9);
+PrintMatrix(array2D);
+Console.WriteLine();
+int[,] result = CreateMatrixChange(array2D);
+PrintMatrix(result);
